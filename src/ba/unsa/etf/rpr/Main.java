ec71ad71c068a19abb665979cae50c6e1d823892
Ema Rudalija <erudalija1@etf.unsa.ba>
2019-10-17 16:07:15 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+public class Main {
+    public static int sumaCifara(int br) {
+        int suma=0;
+        while (br != 0) {
+            suma += br % 10;
+            br /= 10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesi cijeli broj: ");
+        int vel =  ulaz.nextInt():
+        for (int i = 1; i < vel; i++)
+            if (i % sumaCifara(i) == 0)
+                System.out.println(i);
+    }
+}
